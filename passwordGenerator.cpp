60ad4e59aacba2db254e63156cd70708bdf2e9af
Ton-of-Ash <46545360+ashlord101@users.noreply.github.com>
2023-07-31 01:21:14 -0700

    finished the project, need to add safeties for the user input

--- passwordGenerator.cpp
+++ passwordGenerator.cpp
@@ -1,9 +1,31 @@
-// passwordGenerator.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+
+std::string generate(int numOfChars, std::string specialChars);
 
 int main()
 {
+    srand(time(0));
+
+    int numOfChars;
+    std::string specialChars = "";
+    std::cout << "Password Length : " << std::endl;
+    std::cin >> numOfChars;
+    std::cout << "Special Characters Allowed : " << std::endl;
+    std::cin >> specialChars;
+    std::cout << generate(numOfChars, specialChars) << std::endl;
     
+}
+
+std::string generate(int numOfChars, std::string specialChars) {
+
+    std::string chars = "abcdefghijklmnopqrstuvwxyz0123456789" + specialChars;
+    std::string password = "";
+
+    for (int i = 0; i < numOfChars; i++) {
+        password += chars[rand() % (chars.length() - 1)];
+    }
+
+    return password;
+
 }
